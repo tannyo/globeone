ec9f75b9c81afc09f28dbf10120297c9695f8bdc
Tanny O'Haley <tannyo@gmail.com>
2014-12-03 17:56:00 -0800

    Track Join Us, Contact Us, meeting requests, and file download events

---
+++ js/downloads.js
@@ -0,0 +1,15 @@
+/*
+ * Track in Google Analytics when a user downloads a file or requests a meeting.
+ */
+$("#sec-download-files").find("a").on("click", function(event) {
+  var file_type;
+
+  if (this.id === "request-more-info" || this.id === "play-video") {
+    return;
+  } else if (/^mailto/.test(this.href)) {
+    file_type = "mailto";
+  } else {
+    file_type = this.href.substr(this.href.lastIndexOf(".") + 1).toLowerCase();
+  }
+  track_event("downloads", file_type, this.text);
+});
